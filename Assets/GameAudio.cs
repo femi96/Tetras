b455cb3b35a971e2455bf814868d92ca7045f392
Femi Oladipupo <imef96@gmail.com>
2019-05-01 15:20:33 -0400

    Add background music

---
+++ Assets/GameAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudio : MonoBehaviour {
+
+  [Header("Audio")]
+  public AudioSource bgMusic;
+
+  void Start() {
+    bgMusic.volume = 0.1f;
+    bgMusic.Play();
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Minus)) {
+      bgMusic.volume -= bgMusic.volume * 0.1f;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Equals)) {
+      bgMusic.volume += bgMusic.volume * 0.1111f;
+    }
+  }
+}
